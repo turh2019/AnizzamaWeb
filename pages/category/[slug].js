dde7523cf376f8944a9a8dc29b1cadb226409a23
turh2019 <54136458+turh2019@users.noreply.github.com>
2022-01-02 19:01:27 +0200

    Update [slug].js

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -5,11 +5,7 @@ import { getCategories, getCategoryPost } from '../../services/services';
 import { PostCard, Categories, Loader,LinksTo,PostWidget ,Toolbar} from '../../components/getComponents';
 
 const CategoryPost = ({ posts }) => {
-  const router = useRouter();
 
-  if (router.isFallback) {
-   return <Loader />;
- }
 
   return (
     <div className="container mx-auto px-10 mb-8">
