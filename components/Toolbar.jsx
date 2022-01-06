c1d0f82dbc13dea72ab47d7532e5cb0cdb3e3fee
turh2019 <54136458+turh2019@users.noreply.github.com>
2022-01-06 12:49:57 +0200

    Add files via upload

--- components/Toolbar.jsx
+++ components/Toolbar.jsx
@@ -15,7 +15,7 @@ const Toolbar = (type) => {
         <>
         <div className=' bg-contain bg-center bg-no-repeat  pb-3 bg-clip-padding  p-6 grid grid-cols-1 gap-4  place-items-center '>
             <div className='place-self-end flex flex-wrap'>      
-                <div className='place-self-canter  px-3 pb-3 my-2'>
+                <div className='place-self-canter  px-3 pb-3 my-2 gap-5'>
                     {pages.map((page)=>(
                         <a  target="_blank" key={page.name} href={`/pages/${page.slug}`}>
                         <Link target="_blank" key={page.name} href={`/pages/${page.slug}`}>
@@ -26,16 +26,16 @@ const Toolbar = (type) => {
                         </a>
                        
                         ))}
-                        <a target="_blank" key="home" href={`/`}>
+                        <a target="_blank" key="home" href={`/`} className=' '>
                          <Link target="_blank" key="home" href={`/`}>
-                            <span className='transition duration-500 ease hover:bg-[#382C8B] inline-block bg-[#4864F6] text-lg font-medium rounded-full text-white px-8  cursor-pointer  mx-2 ' onClick={e=>Setselected("")}>
+                            <span className='transition duration-500 ease hover:bg-[#382C8B] inline-block bg-[#4864F6] text-lg font-medium rounded-full text-white px-8  cursor-pointer lg:mt-0 mt-4 mx-2 ' onClick={e=>Setselected("")}>
                                     בית
                             </span>
                         </Link>
                         </a>
                 </div>
                 <SearchBar /> 
-                <Categories selected ={selected}  Setselected ={Setselected} type={type}/>
+                <Categories selected ={selected}  Setselected ={Setselected} type={type} />
             </div>  
         </div>
                 
