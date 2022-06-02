9a648802fde1e5fc00357e9a40bb21b50546eb93
JusAusertad <94871514+evar312ga@users.noreply.github.com>
2022-06-03 00:30:02 +0300

    Add files via upload

--- pages/team/[slug].js
+++ pages/team/[slug].js
@@ -47,7 +47,7 @@ const PostDetails = ({post, ep,slugs}) => {
           <meta name="description" content="אניזאמה; אנימות לצפייה ישירה עם כתוביות בעברית!"/>
           <meta property="og:title" content="Anizzama - Jobs"/>
           <meta property="og:description" content="עמוד הדרושים; בעמוד זה תוכלו להבחן ולראות את מגוון התפקידים בפאנסאב."/>
-          <meta property="og:url" content="https://anizzama.vercel.app/team/jobs"/>
+          <meta property="og:url" content="https://anizzama.vercel.app/team/staff"/>
           <meta property="og:image" content={post.featuredImage.url}/>
           <meta property="og:site_name" content="Anizzama"/>
         </Head>
