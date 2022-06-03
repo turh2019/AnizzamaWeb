fac5612abe75a9d50cb85111480ab0b75e39cf6a
JusAusertad <94871514+evar312ga@users.noreply.github.com>
2022-06-03 03:21:16 +0300

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
-    siteUrl: 'https://anizzama.vercel.app/',
-    generateRobotsTxt: true, // (optional)
-    ourDir: "./out",
-  }
