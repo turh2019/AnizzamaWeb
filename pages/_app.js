2a52a762a2268ec7b2387fb9b68bd997f6ae0a00
JusAusertad <94871514+evar312ga@users.noreply.github.com>
2022-06-21 00:28:41 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -37,7 +37,7 @@ function MyApp({ Component, pageProps }) {
           <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
           <base href="/"/>
-          <link rel="icon" type="image/x-icon" href="https://anizzama.com/favicon.ico"/>
+          <link rel="icon" type="image/x-icon" href="https://www.anizzama.com/favicon.ico"/>
           <meta name="mobile-web-app-capable" content="yes"/>
           <meta name="robots" content="index,follow"/>
           <meta name="mobile-web-app-capable" content="yes"/>
