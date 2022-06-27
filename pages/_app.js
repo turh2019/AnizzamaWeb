ad3fe7a60ccb95f469224d1e07e805f5961d8ba7
JusAusertad <94871514+evar312ga@users.noreply.github.com>
2022-06-28 02:34:21 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -37,11 +37,15 @@ function MyApp({ Component, pageProps }) {
           <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
           <base href="/"/>
-          <link rel="icon" type="image/x-icon" href="https://media.graphassets.com/Ohod0HmDREytfDNipJCD"/>
+          <link rel="icon" type="image/x-icon" href="https://anizzama.vercel.app/favicon.ico"/>
           <meta name="mobile-web-app-capable" content="yes"/>
           <meta name="robots" content="index,follow"/>
           <meta name="mobile-web-app-capable" content="yes"/>
           <meta name="application-name" content="Anizzama"/>
+          <meta name="og:email" content="anizzamasub@gmail.com"/>
+          <meta name="og:type" content="fansub"/>
+          <meta name="language" content="HE"/>
+          <meta name="og:locale" content="he_IL"/>
       </Head>
 
       
