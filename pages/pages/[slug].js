c2bfaaa26db4ae5315abc570615285377217be2b
JusAusertad <94871514+evar312ga@users.noreply.github.com>
2022-07-01 01:08:17 +0300

    Add files via upload

---
+++ pages/pages/[slug].js
@@ -0,0 +1,77 @@
+import React,{useState,useEffect} from 'react';
+import { useRouter } from 'next/router';
+import  Head  from 'next/head';
+import { getPages, getPagesPage,getCategories} from '../../services/services';
+import { Loader,LinksTo,PostWidget ,CategoriesSearch,Team,Cooperation} from '../../components/getComponents';
+
+const Pagespage = ({ Pages , params }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+   return <Loader />;
+ }
+
+ var descriptionA;
+ var descriptionB;
+ var titleA;
+
+       if(params.slug === 'anime') {
+        descriptionA = "אניזאמה; אנימות לצפייה ישירה עם כתוביות בעברית!"
+        descriptionB = "אניזאמה; אנימות לצפייה ישירה עם כתוביות בעברית!"
+        titleA = "Anizzama - Anime List"
+       } else if(params.slug === 'team/jobs') {
+        descriptionA = "אניזאמה; אנימות לצפייה ישירה עם כתוביות בעברית!"
+        descriptionB = "עמוד הצוות; בעמוד זה תוכלו לראות את כל צוות האתר ולגשת אל עמוד הדרושים."
+        titleA = "Anizzama - Staff"
+       }
+
+  return (
+    <div className="container mx-auto px-10 mb-8" >
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
+        <Head>
+        <title>{titleA}</title>
+          <meta name="description" content={descriptionA}/>
+          <meta property="og:url" content={"https://www.anizzama.com/pages/"+params.slug}/>
+          <meta property="og:title" content={titleA}/>
+          <meta property="og:description" content={descriptionB}/>
+          <meta property="og:site_name" content="Anizzama"/>
+          <meta property="og:image" content="https://media.graphassets.com/Ohod0HmDREytfDNipJCD"/> 
+        </Head>
+        {params.slug =="team"? <div><Team /></div> :""}
+        {params.slug =="anime"?<div><CategoriesSearch posts={Pages} /></div>:"" }
+        {params.slug =="cooperation"?<div><Cooperation/></div>:"" }
+         
+      </div>
+        <div className="col-span-1 lg:col-span-4 float-left">
+          <div className="relative top-8 ">
+        
+            <PostWidget />
+            
+            <LinksTo />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Pagespage;
+
+// Fetch data at build time
+export async function getStaticProps({ params }) {
+  const Pages = await getPagesPage(params.slug);
+
+  return {
+    props: { Pages, params },
+  };
+}
+
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
+export async function getStaticPaths() {
+  const categories = await getPages();
+  return {
+    paths: categories.map(({ slug }) => ({ params: { slug } })),
+    fallback: true,
+  };
+}
---
+++ pages/post/[slug].js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import  Head  from 'next/head';
+import { PostDetail,Categories, PostWidget, Author, Comments, CommentsFrom,Loader ,LinksTo,Toolbar} from '../../components/getComponents';
+import { getPosts, getPostDetails } from '../../services/services';
+
+
+const PostDetails = ({post}) => {
+  var [selected, Setselected] = useState("")
+
+ const router =useRouter();
+ if(router.isFallback){
+  return <Loader />
+ }
+ var AAAa;
+if(post.title.toLowerCase().includes("anizzama") || post.title.includes("אניזאמה")) AAAa = post.title;
+else AAAa = "Anizzama - " + post.title;
+  return (
+    <>
+      <div className="container mx-auto px-10 mb-8" >
+      <Head>
+      <title>{post.title}</title>
+          <meta property="description" content={post.excerpt}/>
+          <meta property="og:title" content={post.title}/>
+          <meta property="og:description" content={post.excerpt}/>
+          <meta property="og:url" content= {"https://www.anizzama.com/post/"+post.slug}/>
+          <meta property="og:image" content={post.featuredImage.url}/>
+          <meta property="og:site_name" content="Anizzama"/>
+        </Head>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="col-span-1 lg:col-span-8">
+           
+           <PostDetail post={post} />
+
+             <Author  author={post.author} />
+
+            <CommentsFrom slug={post.slug} type="" selected={selected}  Setselected={Setselected}/>
+            <Comments slug={post.slug} from="post" selected={selected}  Setselected={Setselected}  />
+          </div>
+          <div className="col-span-1 lg:col-span-4 float-left" >
+            <div className="relative lg:sticky top-8">
+           
+            <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
+           
+            <LinksTo />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default PostDetails;
+
+
+export async function getStaticProps({ params }) {
+  const data = await getPostDetails(params.slug);
+  
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
+
+
+export async function getStaticPaths() {
+  const posts = await getPosts();
+
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: true,
+  };
+}
